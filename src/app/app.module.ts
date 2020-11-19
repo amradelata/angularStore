068b424733503633d467ac4aba807dd6b95e5b8f
amradelata <amradelata@gmail.com>
2020-11-19 11:18:27 +0200

    nav/carousel/hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {IvyCarouselModule} from 'angular-responsive-carousel';
+import { CarouselComponent } from './carousel/carousel.component';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { MyHeroComponent } from './my-hero/my-hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    MyNavComponent,
+    MyHeroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IvyCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
